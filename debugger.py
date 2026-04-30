b0afc75f2fb7a362b4cafc7a00313dcf61dca0c9
laoda555 <3263742916@qq.com>
2026-04-30 13:26:35 +0800

    Create debugger.py

---
+++ debugger.py
@@ -0,0 +1,8 @@
+class DebugAgent:
+    def __init__(self):
+        self.name = "DebugAgent"
+
+    def fix(self, code):
+        print(f"[{self.name}] 检测并修复代码...")
+        # 模拟修复
+        return code
